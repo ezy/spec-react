191e226a3c4b03ce5b235b2b1a940d098e41ce12
Ezy <ezra@passionfruit.co.nz>
2020-03-31 19:43:32 +1300

    scaffold blog list page

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+
+import { Header } from './Header';
+
+export const Blog = (props) => {
+  const { title } = props;
+  const sections = [{ title: 'Posts' }];
+
+  const initialUserState = {
+    posts: [],
+    loading: true,
+  };
+
+  const [posts, setPosts] = useState(initialUserState);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      setPosts(await response.json());
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <Container maxWidth="lg">
+      <CssBaseline />
+      <Header title={title} sections={sections} />
+      {/* <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/:id" component={UserPage} />
+      </Switch> */}
+      <main>
+        {posts.loading ? (
+          <h2 style={{ marginLeft: 20 }}>Loading posts...</h2>
+        ) : (
+          <Grid container spacing={9} style={{ marginTop: 10 }}>
+            {/* posts go here */}
+          </Grid>
+        )}
+      </main>
+    </Container>
+  );
+};
