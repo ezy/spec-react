80db4e2c0eced569da471df64f58e04f07a4b958
Ezy <ezra@passionfruit.co.nz>
2020-03-31 19:59:49 +1300

    make routes work and load cards into grid

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,56 +2,23 @@ import React, { useEffect, useState } from 'react';
 
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
+import { Switch, Route } from 'react-router-dom';
 
-import { ArticleCard } from '../Card/Card';
+import { CardGrid } from '../CardGrid/CardGrid';
 import { Header } from './Header';
 
 export const Blog = (props) => {
   const { title } = props;
   const sections = [{ title: 'Home' }];
 
-  const initialUserState = {
-    posts: [],
-    loading: true,
-  };
-
-  const [posts, setPosts] = useState(initialUserState);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/posts'
-      );
-      setPosts(await response.json());
-    };
-    fetchPosts();
-  }, []);
-
   return (
     <Container maxWidth="lg">
       <CssBaseline />
       <Header title={title} sections={sections} />
-      {/* <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/:id" component={UserPage} />
-      </Switch> */}
-      <main>
-        {posts.loading ? (
-          <h2 style={{ marginLeft: 20 }}>Loading posts...</h2>
-        ) : (
-          <Grid container spacing={9} style={{ marginTop: 10 }}>
-            {posts &&
-              posts.map((post) => (
-                <ArticleCard
-                  key={post.id}
-                  title={post.title}
-                  body={post.body}
-                />
-              ))}
-          </Grid>
-        )}
-      </main>
+      <Switch>
+        <Route exact path="/" component={CardGrid} />
+        {/* <Route path="/:id" component={UserPage} /> */}
+      </Switch>
     </Container>
   );
 };
---
+++ src/components/CardGrid/CardGrid.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+import Grid from '@material-ui/core/Grid';
+
+import { ArticleCard } from '../Card/Card';
+
+export const CardGrid = () => {
+  const initialUserState = {
+    posts: [],
+    loading: true,
+  };
+
+  const [posts, setPosts] = useState(initialUserState);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      setPosts(await response.json());
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <main>
+      {posts.loading ? (
+        <h2 style={{ marginLeft: 20 }}>Loading posts...</h2>
+      ) : (
+        <Grid container spacing={9} style={{ marginTop: 10 }}>
+          {posts &&
+            posts.map((post) => (
+              <ArticleCard key={post.id} title={post.title} body={post.body} />
+            ))}
+        </Grid>
+      )}
+    </main>
+  );
+};
